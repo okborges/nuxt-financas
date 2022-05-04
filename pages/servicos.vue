60712d534ae0743d8843e5e590e5d92d8e571b30
Rafael Borges <rafaelborges.av@gmail.com>
2022-05-04 18:39:15 -0300

    adicionando rotas

---
+++ pages/servicos.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="bg-gray-100">
+      <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
+        <NuxtLink to="/a-empresa">A Empresa</NuxtLink>
+        <NuxtLink to="/servicos">Serviços</NuxtLink>
+      </div>
+    </div>
+
+    <div>
+      <div class="container mx-auto">
+        <h1 class="text-lg font-bold">Serviços</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+          error quibusdam veritatis amet, voluptas quam quos repudiandae ipsam.
+          Dolorem provident, maiores necessitatibus vero laboriosam asperiores
+          eos facilis nesciunt nihil quod.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
